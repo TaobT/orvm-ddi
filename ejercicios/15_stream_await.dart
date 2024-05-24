2ce51454e4f66292f91007ef7be3eec0cc98cee6
TaobT <ninjotase165@gmail.com>
2024-05-23 18:34:44 -0600

    Ejercicio 15

---
+++ ejercicios/15_stream_await.dart
@@ -0,0 +1,21 @@
+// Streams
+// Son flujos de información que pueden
+// estar emitiendo valores periodicamente
+// una vez o nunca.
+
+// Son como el flujo de agua (el flujo es el stream)
+// puede cerrarse o abrirse
+void main() {
+  emitNumbers().listen((value) {
+    print('emit: $value');
+  });
+}
+
+Stream<int> emitNumbers() async* {
+  final valuesToEmit = [1,2,3,4,5];
+  for(int i in valuesToEmit) {
+    await Future.delayed(const Duration(seconds: 1));
+    //ceder un valor
+    yield i;
+  }
+}
